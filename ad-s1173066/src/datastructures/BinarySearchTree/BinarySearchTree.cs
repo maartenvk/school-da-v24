822e326153ec0a9a30c8efa7c42fa97210762e14
Maarten van Keulen <maartenvkeulen@proton.me>
2024-09-26 19:00:52 +0200

    add insertion function

--- ad-s1173066/src/datastructures/BinarySearchTree/BinarySearchTree.cs
+++ ad-s1173066/src/datastructures/BinarySearchTree/BinarySearchTree.cs
@@ -8,9 +8,47 @@ namespace AD
         // Interface methods that have to be implemented for exam
         //----------------------------------------------------------------------
 
+        public void InsertRecursive(BinaryNode<T> node, T x)
+        {
+            int comparison = x.CompareTo(node.GetData());
+            if (comparison == 0)
+            {
+                return; // equal
+            }
+
+            if (comparison < 0) // left
+            {
+                if (node.HasLeft())
+                {
+                    InsertRecursive(node.GetLeft(), x);
+                }
+                else
+                {
+                    node.left = new BinaryNode<T>(x);
+                }
+            }
+            else // right
+            {
+                if (node.HasRight())
+                {
+                    InsertRecursive(node.GetRight(), x);
+                }
+                else
+                {
+                    node.right = new BinaryNode<T>(x);
+                }
+            }
+        }
+
         public void Insert(T x)
         {
-            throw new System.NotImplementedException();
+            if (IsEmpty())
+            {
+                root = new BinaryNode<T>(x);
+                return;
+            }
+
+            InsertRecursive(GetRoot(), x);
         }
 
         public T FindMin()
